9974da8ca053d5fd141580f9f8b81b8f9e69c509
IlyaChistov <gaplis322@gmail.com>
2022-10-05 00:34:55 +0300

    Finaly programm

--- Program.cs
+++ Program.cs
@@ -33,3 +33,36 @@ string[] SortedArray(string[] array, int count, int lengthWord)
     }
     return newArray;
 }
+
+void PrintArray(string[] array, int lengthWord)
+{
+    if (array.Length == 0)
+    {
+        Console.WriteLine($"В данном массиве нет строк меньше, либо равным {lengthWord} символам.");
+    }
+    else
+    {
+        Console.Write($"Строки меньше, либо равные {lengthWord} символам: ");
+        Console.WriteLine(String.Join(", ", array));
+    }
+}
+
+void PrintTask(string[] array, int lengthWord)
+{
+    Console.Write("Заданный массив: ");
+    Console.WriteLine(String.Join(", ", array));
+    int amountWords = FindAmountWordsInArray(array, lengthWord);
+    string[] sortArray = SortedArray(array, amountWords, lengthWord);
+    PrintArray(sortArray, lengthWord);
+    Console.WriteLine();
+}
+
+string[] array1 = { "Hello", "2", "world", ":-)" };
+string[] array2 = { "1234", "1567", "-2", "computer science" };
+string[] array3 = { "Russia", "Denmark", "Kazan" };
+
+int wordLenth = 3;
+
+PrintTask(array1, wordLenth);
+PrintTask(array2, wordLenth);
+PrintTask(array3, wordLenth);

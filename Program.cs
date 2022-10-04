4aa0402146ee574adc4202093e7ddd1c3c2effef
IlyaChistov <gaplis322@gmail.com>
2022-10-04 23:59:52 +0300

    Added function SortedArray

--- Program.cs
+++ Program.cs
@@ -2,20 +2,34 @@
 // длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
-// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-// [“Russia”, “Denmark”, “Kazan”] → []
+// ["Hello", "2", "world", ":-)"] → ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] → ["-2"]
+// ["Russia", "Denmark", "Kazan"] → []
 
-int FindAmountWordsInArray(string[] array)
+int FindAmountWordsInArray(string[] array, int lengthWord)
 {
     int amount = 0;
-    int wordLength = 3;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= wordLength)
+        if (array[i].Length <= lengthWord)
         {
             amount++;
         }
     }
     return amount;
+}
+
+string[] SortedArray(string[] array, int count, int lengthWord)
+{
+    string[] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; j < newArray.Length; i++)
+    {
+        if (array[i].Length <= lengthWord)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
 }
